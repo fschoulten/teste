50c1132d194e984c758393919c3599fe17b5ebc2
fschoulten <88036356+fschoulten@users.noreply.github.com>
2023-05-29 19:03:21 +0000

    interface

--- app.py
+++ app.py
@@ -2,15 +2,45 @@ from shiny import ui, App
 
 # Interface do usuário (UI)
 app_ui = ui.page_navbar(
+    # Cria uma "página interna" na dashboard
+    ui.nav(
+        # Título da página
+        "Página 1",
+        ui.layout_sidebar(
+            ui.panel_sidebar("Contúdo da barra lateral"),
+            ui.panel_main(
+                ui.row(
+                    ui.column(6, "Bloco A", {"style": "background-color: yellow;"}),
+                    ui.column(6, "Bloco B", {"style": "background-color: red;"})
+                ),
+                ui.row(
+                    ui.column(4, "Bloco C", {"style": "background-color: blue;"}),
+                    ui.column(4, "Bloco D", {"style": "background-color: orange;"}),
+                    ui.column(4, "Bloco E", {"style": "background-color: purple;"})
+                )
+            )
+        )
+    ),
+    ui.nav(
+        # Título da página
+        "Página 2"
+    ),
+    #ui.nav_spacer(),
+    # Links barra de navegação
     ui.nav_control(ui.a("Análise Macro", href = "https://analisemacro.com.br/")),
     ui.nav_menu(
         "Mais",
         ui.nav_control(
             ui.a("LinkedIn", href = "https://br.linkedin.com/company/an%C3%A1lise-macro"),
             ui.a("Blog", href = "https://analisemacro.com.br/blog/")
-            )
         ),
-    title = "Título da dashboard",
+        align="left"
+    ),
+    # Título e estilos barra de navegação
+    title = ui.div(
+        ui.img(src = "https://aluno.analisemacro.com.br/download/49491/?tmstv=1685386850"),
+        " Título da dashboard"
+        ),
     bg = "blue",
     inverse = True
 )
